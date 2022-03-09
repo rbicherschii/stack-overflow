1bbcee420754f2aa4b2393fbdd5cf005aa4c43bb
Roman Bickersky <rbicherschii@gmail.com>
2022-03-09 16:25:21 -0500

    Adding testcase for removeChild (but it's failing)

--- parentChildTest/src/test/java/com/roman/example/parentChildTest/ParentChildTestApplicationTests.java
+++ parentChildTest/src/test/java/com/roman/example/parentChildTest/ParentChildTestApplicationTests.java
@@ -1,12 +1,18 @@
 package com.roman.example.parentChildTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import javax.transaction.Transactional;
 
+import org.hibernate.Hibernate;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
@@ -16,6 +22,7 @@ import com.roman.example.parentChildTest.domain.Parent;
 import com.roman.example.parentChildTest.repository.ChildRepository;
 import com.roman.example.parentChildTest.repository.ParentRepository;
 
+@TestMethodOrder(MethodOrderer.MethodName.class)
 @SpringBootTest
 @Rollback(false)
 class ParentChildTestApplicationTests {
@@ -28,8 +35,8 @@ class ParentChildTestApplicationTests {
 
     @Transactional
 	@Test
-    void addChildParent() {
-        System.out.println("########################################################## RUNNING TEST CASE");
+    void test01_addChild() {
+        System.out.println("########################################################## ADDING CHILD");
 
         // Fetching existing parent by id
         // comes from predefined dataset in src/main/resources/data.sql
@@ -59,8 +66,50 @@ class ParentChildTestApplicationTests {
         final Child childVerify = childRepo.getById(childId);
         assertNotNull(childVerify);
 
-        System.out.println("########################################################## COMPLETING TEST CASE");
+        // Finally making sure that parent.children are not lazy initialized
+        assertFalse(Hibernate.isInitialized(parent.getChildren()));
+
+        System.out.println("########################################################## DONE ADDING CHILD");
 
 	}
+
+    @Transactional
+    @Test
+    void test02_removeChild() {
+        System.out.println("########################################################## REMOVING CHILD");
+
+        // Fetching existing parent by id
+        // comes from predefined dataset in src/main/resources/data.sql
+        final Long parentId = 4291893511L;
+        final Parent parent = parentRepo.getById(parentId);
+
+        assertNotNull(parent);
+
+        // Fetching child we want to remove by id
+        final Long childToRemoveId = 4291893513L;
+        final Child childToRemove = childRepo.getById(childToRemoveId);
+
+        assertNotNull(childToRemove);
+
+        // Now trying to remove a child (the goal is not to load p.children in memory)
+        parent.getChildren()
+                .remove(childToRemove);
+
+        childRepo.delete(childToRemove);
+        parentRepo.save(parent);
+
+        childRepo.flush();
+
+        System.out.println("Verification:");
+        // And making sure that child was created
+        final Optional<Child> childVerify = childRepo.findById(childToRemoveId + 100);
+        assertTrue(childVerify.isEmpty());
+
+        // Finally making sure that parent.children are not lazy initialized
+        assertFalse(Hibernate.isInitialized(parent.getChildren()));
+
+        System.out.println("########################################################## DONE REMOVING CHILD");
+
+    }
 
 }
